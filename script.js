19e23aa0c6a4b6b6656247f33b34eca660afdd07
brucecorrea0 <brucecorrea099@gmail.com>
2026-07-18 17:35:51 +0600

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function add() {
-    let a = Number(document.getElementById("a").value);
-    let b = Number(document.getElementById("b").value);
-
-    document.getElementById("result").innerHTML =
-        "Result = " + (a + b);
-}
